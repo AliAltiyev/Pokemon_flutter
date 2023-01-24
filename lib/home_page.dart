ccbfb54a5f390ba63fa6e918adefca783fd405da
AliAltiyev <alidroid696@gmail.com>
2023-01-24 23:12:21 +0300

    get data from Api

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:work_with_api/data/poke_service.dart';
+
+import 'model/pokemon.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,10 +11,43 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late Future<List<PokemonModel>> pokeList;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    pokeList = PokeService.getPokemons();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(),
+      body: FutureBuilder(
+        future: pokeList,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+                itemCount: snapshot.data?.length,
+                itemBuilder: (context, index) {
+              final data = snapshot.data![index];
+              return Material(
+                child: SizedBox(
+                  height: 100,
+                  width: 200,
+                  child: Column(
+                    children: [Text(data.avgSpawns.toString())],
+                  ),
+                ),
+              );
+            });
+          } else if (snapshot.hasError) {
+            return  Text(snapshot.hasData.toString());
+          } else {
+            return const CircularProgressIndicator();
+          }
+        },
+      ),
     );
   }
 }
